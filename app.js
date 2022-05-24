fdc5edf327ce4c8422214c1f6e691eb1a3df653e
andrevdal <andreas.drevdal@gmail.com>
2022-05-24 21:45:07 +0200

    finished product for now

--- app.js
+++ app.js
@@ -1,33 +1,47 @@
 let tdEl = document.querySelectorAll("td");
-let methodUsed = false
-let firstNumber = ""
-let secondNumber = ""
-let calcMethod = ""
+let resultEl = document.querySelector(".result");
+let methodUsed = false;
+let firstNumber = "";
+let secondNumber = "";
+let calcMethod = "";
 
-function addNum(digit){
-  if(methodUsed === false){
-    firstNumber = firstNumber + digit
+function addNum(digit) {
+  if (methodUsed === false) {
+    firstNumber = firstNumber + digit;
+    displayNumbers(firstNumber);
   } else {
-    secondNumber = secondNumber + digit
+    secondNumber = secondNumber + digit;
+    displayNumbers(secondNumber);
   }
 }
-function calculate (){
-  console.log(eval(firstNumber + calcMethod + secondNumber));
-  methodUsed = false
-  firstNumber = ""
-  secondNumber = ""
-  calcMethod = ""
+function calculate() {
+  displayNumbers(eval(firstNumber + calcMethod + secondNumber));
+  methodUsed = false;
+  firstNumber = "";
+  secondNumber = "";
+  calcMethod = "";
+}
+function displayNumbers(numbersDisplayed) {
+  resultEl.innerHTML = numbersDisplayed;
 }
 tdEl.forEach((el) => {
-  el.addEventListener("click", (e) => {
+el.addEventListener("click", (e) => {
     let char = e.target.innerText;
     if (isNaN(char)) {
       if (char == "=") {
         calculate();
       } else {
-        if(!methodUsed){
-          calcMethod = char
-          methodUsed = true
+        if (char == "C") {
+          methodUsed = false;
+          firstNumber = "";
+          secondNumber = "";
+          calcMethod = "";
+          displayNumbers("")
+        } else {
+          if (!methodUsed) {
+            calcMethod = char;
+            methodUsed = true;
+          }
         }
       }
     } else {
