1e552081bb7af9f22e042e62aae6b96aa572ce3a
andrevdal <andreas.drevdal@gmail.com>
2022-05-24 20:50:47 +0200

    saving progress, it works

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
 let tdEl = document.querySelectorAll("td");
+let methodUsed = false
+let firstNumber = ""
+let secondNumber = ""
+let calcMethod = ""
 
+function addNum(digit){
+  if(methodUsed === false){
+    firstNumber = firstNumber + digit
+  } else {
+    secondNumber = secondNumber + digit
+  }
+}
+function calculate (){
+  console.log(eval(firstNumber + calcMethod + secondNumber));
+  methodUsed = false
+  firstNumber = ""
+  secondNumber = ""
+  calcMethod = ""
+}
 tdEl.forEach((el) => {
-    el.addEventListener("click", (e) => {
-    let firstNr = ""
-    let calcMeth = ""
-    let secondNr = ""
-    let char = e.target.innerText
-    console.log(isNaN(char));
+  el.addEventListener("click", (e) => {
+    let char = e.target.innerText;
+    if (isNaN(char)) {
+      if (char == "=") {
+        calculate();
+      } else {
+        if(!methodUsed){
+          calcMethod = char
+          methodUsed = true
+        }
+      }
+    } else {
+      addNum(char);
+    }
   });
 });
